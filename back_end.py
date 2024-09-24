06f750604efbb710b21c4c49c710d321fa0137ec
freyzsaycon <47387440+freyzsaycon@users.noreply.github.com>
2024-09-24 13:20:11 +0200

    Changed back end

--- back_end.py
+++ back_end.py
@@ -2,26 +2,27 @@ def leggSammen():
     tall1 = input("Skriv inn det første tallet: ")
     tall2 = input("Skriv inn det andre tallet: ")
     sum = int(tall1) + int(tall2)
-    print(f'{tall2} + {tall2} = {sum}')
+    print(f'{tall1} + {tall2} = {sum}')
 
 
 def trekkFra():
     tall1 = input("Skriv inn det første tallet: ")
     tall2 = input("Skriv inn det andre tallet: ")
     diff = int(tall1) - int(tall2)
-    print(f'{tall2} - {tall2} = {diff}')
+    print(f'{tall1} - {tall2} = {diff}')
 
 def gangSammen():
     tall1 = input("Skriv inn det første tallet: ")
     tall2 = input("Skriv inn det andre tallet: ")
-    diff = int(tall1) - int(tall2)
-    print(f'{tall2} * {tall2} = {diff}')
+    diff = int(tall1) * int(tall2)
+    print(f'{tall1} * {tall2} = {diff}')
 
 def delPaa():
     tall1 = input("Skriv inn det første tallet: ")
     tall2 = input("Skriv inn det andre tallet: ")
-    diff = int(tall1) - int(tall2)
-    print(f'{tall2} / {tall2} = {diff}')
+    diff = int(tall1) / int(tall2)
+    print(f'{tall1} / {tall2} = {diff}')
+
  
 ''' Her er noen påbegynte tanker til utvidelse
 def gange():
